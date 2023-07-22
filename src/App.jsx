088a33ad2ca29bef25056036a79e39f4fab3fca2
Rubcs <Rubcs@gmail.com>
2023-07-22 15:59:24 +0200

    add useState, useEffect and fetchs and styles to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [catFact, setCatFact] = useState("");
+  const [gif, setGif] = useState("");
+
+  const GIPHI_API_KEY = "ePjhJ6oxJijWM5OtKCssAaNGNnTSkfa4";
+
+  const getCatFact = async () => {
+    const result = await fetch("https://catfact.ninja/fact");
+
+    const res = await result.json();
+    setCatFact(res.fact);
+  };
+
+  const stringForGif = catFact.split(" ").slice(0, 3).join(" ");
+
+  const getGif = async () => {
+    const result = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${GIPHI_API_KEY}&q=${stringForGif}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    );
+
+    const res = await result.json();
+    setGif(res.data[0]?.images.original.url);
+    console.log("gif", gif);
+  };
+
+  useEffect(() => {
+    if (catFact) {
+      getGif();
+    }
+  }, [catFact]);
+
+  const handleClick = () => {
+    getCatFact();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="buttonContainer">
+        <h1>CLICK</h1>
+        <button onClick={() => handleClick()}>HERE</button>
+        <h1>AND GET A CATFACT AND A GIF!</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <section className="catGifContainer">
+        <img src={gif} />
+        <div className="factText">
+          <p>{catFact}</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default App;
